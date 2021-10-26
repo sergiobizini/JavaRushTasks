0ae0a0f9c7dfc69ace4bce23024dce8acb223586
sergiobizini <sergiobizini@gmail.com>
2021-10-26 21:27:14 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+    }
+    public static void universalMethod(int i){
+    }
+    public static void universalMethod(int i, int j){
+    }
+    public static void universalMethod(int i, int j, int k){
+    }
+    public static void universalMethod(String s){
+    }
+    public static void universalMethod(String s1, String s2){
+    }
+    public static void universalMethod(String s1, String s2, String s3){
+    }
+    public static void universalMethod(int i, String s){
+    }
+    public static void universalMethod(int i, int j, String s){
+    }
+    public static void universalMethod(String s1, String s2, int i){
+    }
+
+    //напишите тут ваш код
+}
